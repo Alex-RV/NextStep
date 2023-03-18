a9185746896ee58d4086308a0ceeee367fde447d
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-03-18 11:20:30 -0700

    alex's problem 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Container from './components/Container'
+import React from 'react'
 
 export default function Home() {
   return (
