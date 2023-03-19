dc3ab09c9408e23895791f47f262449d45c8ddf8
dav <usr21p1@gmail.com>
2023-03-18 19:28:28 -0700

    some

--- src/pages/api/newdb/mod.ts
+++ src/pages/api/newdb/mod.ts
@@ -1,9 +1,38 @@
 type UserId = string;
 
+enum Interests {
+  "Agriculture",
+  "Art",
+  "Architecture",
+  "Biology",
+  "Business",
+  "Chemistry",
+  "Geography",
+  "Government",
+  "History",
+  "Law",
+  "Communications",
+  "ComputerScience",
+  "Economics ",
+  "English",
+  "Engineering",
+  "Finance",
+  "Management",
+  "Marketing",
+  "Mathematics",
+  "Physics",
+}
+
 interface DatabaseEntry {
   name: string;
-  interests: string[];
+  interests: Interests[];
   goals: string;
+  pronouns: string;
+  phone: string;
+  education: string,
+  experience: string,
+  residence: string,
+  birth: string,
 }
 
 interface Database {
