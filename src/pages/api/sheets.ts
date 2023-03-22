b5f8c060c97a1f0c8ffb4e58788c5cdd8dcf42fd
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-03-22 10:35:35 -0700

    testing team page with sheets

--- src/pages/api/sheets.ts
+++ src/pages/api/sheets.ts
@@ -15,28 +15,38 @@ type SheetForm={
     interests: string[]
 }
 
-export async function addDataToSheet(req: NextApiRequest,res: NextApiResponse) {
-
-    if(req.method !== 'POST'){
-        return res.status(405).send({message: 'Only "POST"'})
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'POST') {
+        return res.status(405).send({ message: 'Only POST requests allowed' })
     }
 
-    const body = req.body as SheetForm;
+    const body = req.body as SheetForm
 
     try {
-        const auth = await google.auth.getClient({
+        const auth = new google.auth.GoogleAuth({
             credentials: {
-                client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-                private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY?.replace(/\\n/g, '\n')
+                client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAILL,
+                private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
             },
-            scopes: ['https://www.googleapis.com/auth/spreadsheets']
+            scopes: [
+                'https://www.googleapis.com/auth/drive',
+                'https://www.googleapis.com/auth/drive.file',
+                'https://www.googleapis.com/auth/spreadsheets'
+            ]
+        })
+
+        const sheets = google.sheets({
+            auth,
+            version: 'v4'
         });
 
-        const sheets = google.sheets({ version: 'v4', auth });
         const response = await sheets.spreadsheets.values.append({
-            spreadsheetId: process.env.SPREADSHEET_ID,
-            range: 'A1:L1',
-            valueInputOption: "USER_ENTERED",
+            spreadsheetId: process.env.NEXT_PUBLIC_SPREADSHEET_NAME,
+            range: 'A1:K1',
+            valueInputOption: 'USER_ENTERED',
             requestBody: {
                 values: [
                     [
@@ -52,14 +62,15 @@ export async function addDataToSheet(req: NextApiRequest,res: NextApiResponse) {
                         body.goals,
                         body.interests.join(', '), // Join the interests array into a comma-separated string
                     ],
-                ],
-            },
+                ]
+            }
         });
-        console.log("SOmething happened")
-        return res.status(200).json({data:response.data})
 
-    } catch (e) {
-        console.error(e)
-        return res.status(500).send({message:'something went wrong'})
+        return res.status(201).json({
+            data: response.data
+        })
+    }catch (e) {
+        return res.status(e.code).send({message: e.message})
     }
+
 }
